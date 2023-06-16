da93871f623888686aeaff36d585c781c8c0c868
soonable@naver.com <soonable@naver.com>
2023-06-16 18:16:46 +0900

    map

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./App.css";
 
 export default class App extends Component {
+
+  btnStyle = {
+    color: "#000080",
+    border: "none",
+    padding: "5px 9px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    float: "right"
+  }
+
+  listStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: "none"
+    }
+  }
+
+  todoData = [
+    {
+      id: "1",
+      title: "공부하기",
+      completed: true
+    },
+    {
+      id: "2",
+      title: "청소하기",
+      completed: false
+    }
+  ]
+
   render() {
-    return(
+    return (
       <div className="container">
         <div className="todoBlock">
           <div className="title">
             <h1>할 일 목록</h1>
           </div>
+
+          {this.todoData.map(data => (
+            <div style={this.listStyle()} key={data.id}>
+              <input type="checkbox" defaultChecked={false} />
+              {data.title}
+              <button style={this.btnStyle}>x</button>
+            </div>
+          ))}
+
         </div>
       </div>
     )
