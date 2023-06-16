8d5e0f51afb6e1900c25dd756e2ef70d961c8d15
soonable@naver.com <soonable@naver.com>
2023-06-16 17:20:13 +0900

    todo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, {Component} from "react";
+import "./App.css";
 
 export default class App extends Component {
   render() {
     return(
-      <div>
-        Hello!!
+      <div className="container">
+        <div className="todoBlock">
+          <div className="title">
+            <h1>할 일 목록</h1>
+          </div>
+        </div>
       </div>
     )
   }
